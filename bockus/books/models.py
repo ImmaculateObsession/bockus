966ca98ec4665b7c4d171b85c3ff920fe3140d85
Kelsey Hawley <k.karin.hawley@gmail.com>
2014-09-13 00:16:12 -0700

    adding models to books, some testing already to confirm working

--- bockus/books/models.py
+++ bockus/books/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+COVER_TYPE_CHOICES = (
+    ("Front", "Front"),
+    ("Spine", "Spine"),
+    ("Back", "Back"),
+    ("Full", "Full - Front, Spine & Back"),
+)
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    author_first_name = models.CharField(max_length=25)
+    author_last_name = models.CharField(max_length=25)
+    summary = models.TextField()
+
+    def __str__(self):
+        return self.title + " by " + self.author_last_name + ", " + self.author_first_name
+
+class Cover(models.Model):
+    book = models.ForeignKey(Book)
+    publisher = models.CharField(max_length=25)
+    publish_date = models.DateField()
+    # image = models.ImageField(upload_to="book_covers")
+    type = models.CharField(max_length=10, choices=COVER_TYPE_CHOICES)
+
+    def __str__(self):
+        return self.type + " - "+ self.book.title + " by " + \
+            self.book.author_last_name + ", " + self.book.author_first_name
+
